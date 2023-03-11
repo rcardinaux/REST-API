7a345d364d69abfa8fdd8a6d9b71225ebc2c7403
rcardinaux <raphael.cardinaux@gmail.com>
2023-03-11 23:14:05 +0100

    real python lesson

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import render_template # Remove: import Flask
+import connexion
+
+app = connexion.App(__name__, specification_dir="./")
+app.add_api("swagger.yml")
+
+# C onnect the URL route "/" to the home() function
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+""" In short, this code gets a basic web server up and running and makes it respond with a home.html template,
+which will be served to a browser when navigating to the URL "/".
+"""
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000, debug=True)
+
+# When you run app.py, a web server will start on port 8000
