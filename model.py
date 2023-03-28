bbf8cf0e8e9dd81424c6358ca7ce82447e20fb4b
rcardinaux <raphael.cardinaux@gmail.com>
2023-03-28 21:52:54 +0200

    End of part 2

--- model.py
+++ model.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from config import db
+from config import db, ma
 
 class Person(db.Model):
     __tablename__ = "person"
@@ -9,3 +9,12 @@ class Person(db.Model):
     timestamp = db.Column(
         db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow
     )
+# This is how Marshmallow finds attributes in the Person class and learns the types of those attributes so it knows how to serialize and deserialize them.
+class PersonSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Person
+        load_instance = True
+        sqla_session = db.session
+
+person_schema = PersonSchema()
+people_schema = PersonSchema(many=True)
