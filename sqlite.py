f56a137f27c3c65bb8dc0b92ad3ca8b09cc5d2f5
rcardinaux <raphael.cardinaux@gmail.com>
2023-03-28 20:38:25 +0200

    part 2

---
+++ sqlite.py
@@ -0,0 +1,28 @@
+import sqlite3
+conn = sqlite3.connect("people.db")
+columns = [
+    "id INTEGER PRIMARY KEY",
+    "lname VARCHAR UNIQUE",
+    "fname VARCHAR",
+    "timestamp DATETIME",
+]
+create_table_cmd = f"CREATE TABLE person ({','.join(columns)})"
+conn.execute(create_table_cmd)
+# -----------------------------------------------------------------
+people = [
+    "1, 'Fairy', 'Tooth', '2022-10-08 09:15:10'",
+    "2, 'Ruprecht', 'Knecht', '2022-10-08 09:15:13'",
+    "3, 'Bunny', 'Easter', '2022-10-08 09:15:27'",
+]
+for person_data in people:
+    insert_cmd = f"INSERT INTO person VALUES ({person_data})"
+    conn.execute(insert_cmd)
+
+conn.commit()
+# -----------------------------------------------------------------
+cur = conn.cursor()
+cur.execute("SELECT * FROM person")
+
+people = cur.fetchall()
+for person in people:
+    print(person)
